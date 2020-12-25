f7343a20a926c17977439e8be2861bf6e6fafd4e
MeinHserhT <50958253+MeinHserhT@users.noreply.github.com>
2020-12-25 10:07:46 +0700

    Add files via upload

---
+++ src/main/java/com/example/ghienphim/HomeScreen.kt
@@ -0,0 +1,47 @@
+package com.example.ghienphim
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import android.widget.PopupMenu
+import kotlinx.android.synthetic.main.activity_home_screen.*
+import kotlinx.android.synthetic.main.activity_register.*
+
+class HomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_screen)
+
+        btn_theloai.setOnClickListener{
+            val popup=PopupMenu(this,btn_theloai)
+            popup.inflate(R.menu.list_popup_window_item)
+            popup.setOnMenuItemClickListener{
+                Toast.makeText(this, "Item : "+it.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+            popup.show()
+        }
+
+        btn_quocgia.setOnClickListener{
+            val popup=PopupMenu(this,btn_quocgia)
+            popup.inflate(R.menu.show_quoc_gia)
+            popup.setOnMenuItemClickListener{
+                Toast.makeText(this, "Item : "+it.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+            popup.show()
+        }
+
+        btn_nam.setOnClickListener{
+            val popup=PopupMenu(this,btn_nam)
+            popup.inflate(R.menu.show_quoc_gia)
+            popup.setOnMenuItemClickListener{
+                Toast.makeText(this, "Item : "+it.title, Toast.LENGTH_SHORT).show()
+                true
+            }
+            popup.show()
+        }
+    }
+}
+
---
+++ src/main/java/com/example/ghienphim/Option.kt
@@ -0,0 +1,19 @@
+package com.example.ghienphim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_option.*
+import android.content.Intent
+
+class Option : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_option)
+        btn_dangky.setOnClickListener{
+            val intent=Intent(this, Register::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
