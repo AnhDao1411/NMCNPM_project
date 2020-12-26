0ec86c91b0081b4764c6750a16ddafa6d681052e
MeinHserhT <50958253+MeinHserhT@users.noreply.github.com>
2020-12-26 17:31:47 +0700

    Add files via upload

---
+++ src/main/java/com/example/ghienphim/ChatBox.kt
@@ -0,0 +1,11 @@
+package com.example.ghienphim
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChatBox : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_box)
+    }
+}
