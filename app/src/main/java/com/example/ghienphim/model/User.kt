dd3f8f2e55cc4d9210d926342130a89a1baa1a47
NT <>
2021-01-14 16:33:31 +0700

    beta_changePass

--- app/src/main/java/com/example/ghienphim/model/User.kt
+++ app/src/main/java/com/example/ghienphim/model/User.kt
@@ -1,5 +1,6 @@
 package com.example.ghienphim.model
 import com.google.firebase.database.IgnoreExtraProperties
-data class User(val id: Int, val name:String, var pass: String, val email:String, val age: Int)
+
+public data class User(val id: Int, var name:String, var pass: String, val email:String, val age: Int)
 
 
