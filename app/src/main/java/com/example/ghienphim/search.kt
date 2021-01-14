fa8ff58db1fd6a2454cbf8db587c7d8d830b9e50
Nguyeen Trusc <18127055@student.hcmus.edu.vn>
2021-01-15 02:01:15 +0700

    RecycleView

--- app/src/main/java/com/example/ghienphim/search.kt
+++ app/src/main/java/com/example/ghienphim/search.kt
@@ -3,14 +3,28 @@ package com.example.ghienphim
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.ghienphim.model.FilmAdapter
+import com.example.ghienphim.model.FilmsModel
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_favorite.*
 import kotlinx.android.synthetic.main.activity_search.*
 
 class search : AppCompatActivity() {
+    private val db:FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val collectionReference:CollectionReference = db.collection("Films")
+    var filmAdapter: FilmAdapter ?=null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        setUpRecyclerview()
         //link to HomeScreen
         return_btn4.setOnClickListener {
             val intent = Intent(this, HomeScreen::class.java)
@@ -18,48 +32,37 @@ class search : AppCompatActivity() {
             finish()
         }
 
-        search_1.setOnClickListener {
-            val intent = Intent(this, Description::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        search_2.setOnClickListener {
-            val intent = Intent(this, description_12::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        search_3.setOnClickListener {
-            val intent = Intent(this, description_vd::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        search_4.setOnClickListener {
-            val intent = Intent(this, Description2::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        search_5.setOnClickListener {
-            val intent = Intent(this, Description::class.java)
-            startActivity(intent)
-            finish()
-        }
-
-        search_6.setOnClickListener {
-            val intent = Intent(this, description_12::class.java)
-            startActivity(intent)
-            finish()
-        }
+//        search_1.setOnClickListener {
+//            val intent = Intent(this, Description::class.java)
+//            startActivity(intent)
+//            finish()
+//        }
 
         home_search.setOnClickListener {
             val intent = Intent(this, HomeScreen::class.java)
             startActivity(intent)
             finish()
         }
+    }
 
+    fun setUpRecyclerview(){
+        val query: Query = collectionReference
+        val firestoreRecyclerOptions: FirestoreRecyclerOptions<FilmsModel> = FirestoreRecyclerOptions.Builder<FilmsModel>()
+            .setQuery(query, FilmsModel::class.java).build()
+        filmAdapter = FilmAdapter(firestoreRecyclerOptions)
 
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = filmAdapter
+    }
+
+    override fun onStart()
+    {
+        super.onStart()
+        filmAdapter!!.startListening()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        filmAdapter!!.stopListening()
     }
 }
